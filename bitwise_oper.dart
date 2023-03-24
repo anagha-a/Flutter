752043c5bebf21b9c8dfda70dca66eb0e2f5f1ec
Anagha-4 <anaghamanojkumar2017@gmail.com>
2023-03-24 19:15:26 +0530

    operators

---
+++ bitwise_oper.dart
@@ -0,0 +1,13 @@
+ void main() {
+  int a=3;
+  int b=8;
+
+  print(a&b);
+  print(a|b);
+  print(a^b);
+  
+}
+//output
+// 0
+// 11
+// 11
---
+++ type_test_oper.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  String name="Anagha";
+  int a=10;
+
+  print(name is String);
+  print(name is! int);
+  print(a is String);
+  print(a is! int);
+
+}
